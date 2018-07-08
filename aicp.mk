003f9efd7c468112809aaf4a3236137648f96f04
AndroPlus <mail@andro.plus>
2018-07-08 15:02:54 +0900

    Enable Snap Camera for AICP

--- aicp.mk
+++ aicp.mk
@@ -2,3 +2,7 @@ $(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 $(call inherit-product, device/aicp/sepolicy/common/sepolicy.mk)
 -include vendor/lineage/build/core/config.mk
 -include vendor/lineage/build/core/apicheck.mk
+
+# Camera
+PRODUCT_PACKAGES += \
+	Snap
