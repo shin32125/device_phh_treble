2ff328206cfb71bc031f1b349fbdcd37be2d7ced
AndroPlus <mail@andro.plus>
2018-08-12 15:16:33 +0900

    Patch for RRO

--- aicp.mk
+++ aicp.mk
@@ -3,6 +3,12 @@ $(call inherit-product, device/aicp/sepolicy/common/sepolicy.mk)
 -include vendor/lineage/build/core/config.mk
 -include vendor/lineage/build/core/apicheck.mk
 
+PRODUCT_ENFORCE_RRO_TARGETS := framework-res
+
+# Don't enforce privapp-permissions whitelist
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.control_privapp_permissions=log
+
 # Camera
 PRODUCT_PACKAGES += \
 	Snap
