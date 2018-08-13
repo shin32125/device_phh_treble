456fdddb6d7e6d54a45a5424534305b1ce2435a2
AndroPlus <mail@andro.plus>
2018-08-13 15:09:30 +0900

    Revert rro patch

--- aicp.mk
+++ aicp.mk
@@ -2,12 +2,6 @@ $(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 $(call inherit-product, device/aicp/sepolicy/common/sepolicy.mk)
 -include vendor/aicp/build/core/config.mk
 
-PRODUCT_ENFORCE_RRO_TARGETS := framework-res
-
-# Don't enforce privapp-permissions whitelist
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.control_privapp_permissions=log
-
 # Camera
 PRODUCT_PACKAGES += \
 	Snap
