acc5ac18a3f3ed0b403cb51761d40d3a23e20e3f
AndroPlus <mail@andro.plus>
2018-08-13 11:29:27 +0900

    Fix AICP include

--- aicp.mk
+++ aicp.mk
@@ -1,7 +1,6 @@
 $(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 $(call inherit-product, device/aicp/sepolicy/common/sepolicy.mk)
--include vendor/lineage/build/core/config.mk
--include vendor/lineage/build/core/apicheck.mk
+-include vendor/aicp/build/core/config.mk
 
 PRODUCT_ENFORCE_RRO_TARGETS := framework-res
 
