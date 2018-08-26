b5deaec2e8ea17cc19eba19e6277108c3a14d7f8
AndroPlus <mail@andro.plus>
2018-08-26 14:19:09 +0900

    Add LineageOS hardware to support tap to wake for Mi Pad 4

--- aicp.mk
+++ aicp.mk
@@ -2,6 +2,8 @@ $(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 $(call inherit-product, device/aicp/sepolicy/common/sepolicy.mk)
 -include vendor/aicp/build/core/config.mk
 
+BOARD_HARDWARE_CLASS += device/phh/treble/lineagehw
+
 # Camera
 PRODUCT_PACKAGES += \
 	Snap
