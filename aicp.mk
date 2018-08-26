1823ffc1728d4481d6d258f1818bc15250bbdb7e
AndroPlus <mail@andro.plus>
2018-08-26 18:48:01 +0900

    Add tap to wake support for Mi Pad 4

--- aicp.mk
+++ aicp.mk
@@ -2,6 +2,8 @@ $(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 $(call inherit-product, device/aicp/sepolicy/common/sepolicy.mk)
 -include vendor/aicp/build/core/config.mk
 
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc/c177000.i2c/i2c-3/3-0038/fts_gesture_mode"
+
 # Camera
 PRODUCT_PACKAGES += \
 	Snap
