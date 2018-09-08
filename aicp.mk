e9d8afc3c43604685e9460d1bbeff127884e7250
AndroPlus <mail@andro.plus>
2018-09-08 18:17:43 +0900

    Don't enforce privapp-permissions whitelist

--- aicp.mk
+++ aicp.mk
@@ -2,7 +2,9 @@ $(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 $(call inherit-product, device/aicp/sepolicy/common/sepolicy.mk)
 -include vendor/aicp/build/core/config.mk
 
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc/c177000.i2c/i2c-3/3-0038/fts_gesture_mode"
+# Don't enforce privapp-permissions whitelist
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.control_privapp_permissions=log
 
 # Camera
 PRODUCT_PACKAGES += \
