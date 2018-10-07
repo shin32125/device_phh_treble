5e171d95438d32cb8fab29d042a9a8c1b963ae10
AndroPlus <mail@andro.plus>
2018-10-07 12:59:56 +0900

    Fix tablet detection

--- change-device-type.sh
+++ change-device-type.sh
@@ -22,13 +22,14 @@ modify_on_match() {
         -e "s/ro.build.characteristics=.*/ro.build.characteristics=${characteristics}/" \
         /system/build.prop
 
+        if [ "${characteristics}" = "tablet" ]; then
+            sed -i "1i lockscreen.rot_override=true" /system/build.prop
+            sed -i "1i config.force_bottom_navbar=1" /system/build.prop
+        fi
+
         echo "Device type changed! Match: $2 $3"
     fi
 
-    if [ "${characteristics}" = "tablet" ]; then
-        sed -i "1i lockscreen.rot_override=true" /system/build.prop
-        sed -i "1i config.force_bottom_navbar=1" /system/build.prop
-    fi
 }
 
 mount -o remount,rw /system
