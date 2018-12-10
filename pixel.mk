4064be055a68c769f32e29bb55fb1fd8751d3c2e
AndroPlus <mail@andro.plus>
2018-12-10 14:19:56 +0900

    Add PE makefile

---
+++ pixel.mk
@@ -0,0 +1,5 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk) 
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
