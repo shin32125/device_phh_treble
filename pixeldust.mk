2b8d3807fd79f737a42d329821848b8802392eb6
AndroPlus <mail@andro.plus>
2018-12-30 13:20:16 +0900

    Add GoogleCameraMod for Pixel Dust

--- pixeldust.mk
+++ pixeldust.mk
@@ -5,3 +5,7 @@ $(call inherit-product, vendor/pixeldust/configs/system_optional.mk)
 $(call inherit-product, vendor/googleapps/googleapps.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
+
+# Camera
+PRODUCT_PACKAGES += \
+	GoogleCameraMod
