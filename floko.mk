0785e3ce19990d720877553d0cca02b4c668724f
AndroPlus <mail@andro.plus>
2019-01-26 15:00:00 +0900

    temp: ignore SEPolicy neverallows for Floko

--- floko.mk
+++ floko.mk
@@ -1,6 +1,8 @@
 $(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 -include vendor/aicp/build/core/config.mk
 
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # Camera
 PRODUCT_PACKAGES += \
 	Snap
