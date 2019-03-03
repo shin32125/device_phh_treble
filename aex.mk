27c58a59e2f6a4eb3ece45c235d33a206c041d5c
AndroPlus <mail@andro.plus>
2019-03-03 14:30:10 +0900

    Update AEX

--- aex.mk
+++ aex.mk
@@ -2,3 +2,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/aosp/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_RIL_VARIANT := caf
+
+# GoogleCamera
+PRODUCT_PACKAGES += \
+	GoogleCameraMod
+
+# GApps
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+include vendor/gapps/config.mk
