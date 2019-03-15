be7095f7706efd0b9804f4e23207c9f3d3ef545a
AndroPlus <mail@andro.plus>
2019-03-15 22:47:26 +0900

    Fix derp for new FlokoROM

--- floko.mk
+++ floko.mk
@@ -1,5 +1,6 @@
-$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
-$(call inherit-product, device/crdroid/sepolicy/common/sepolicy.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/crdroid/build/core/config.mk
 
 SELINUX_IGNORE_NEVERALLOWS := true
 
