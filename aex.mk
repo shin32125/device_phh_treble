aea98ac18788871b1cd13fa077623b72fb6052e2
AndroPlus <mail@andro.plus>
2019-03-16 09:21:21 +0900

    Revert "Add sap-api-java-static"
    This reverts commit 2387f67fcc54e49859e05ad6ffc6a75cea00a832.

--- aex.mk
+++ aex.mk
@@ -1,3 +1,5 @@
+TARGET_RIL_VARIANT := caf
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/aosp/common.mk)
@@ -6,9 +8,6 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # GoogleCamera
 PRODUCT_PACKAGES += \
 	GoogleCameraMod
-
-PRODUCT_PACKAGES += \
-	sap-api-java-static
 
 # GApps
 TARGET_GAPPS_ARCH := arm64
