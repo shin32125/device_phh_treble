ba69253395d8fdaccd5ada3ab6b03cea4d51b6e8
AndroPlus <hosigumayuugi@gmail.com>
2021-07-25 10:36:50 +0900

    Update LineageOS mk

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,15 @@
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
 -include vendor/lineage/build/core/config.mk
 -include vendor/lineage/build/core/apicheck.mk
-
 PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Fake Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+KERNEL_LD := LD=ld.lld
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := sunfish_defconfig
+TARGET_KERNEL_SOURCE := kernel/google/sunfish
+TARGET_NEEDS_DTBOIMAGE := true
