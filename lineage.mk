d1c5067e2ee993719c993e0603010e51ccfa24f1
AndroPlus <hosigumayuugi@gmail.com>
2022-02-20 14:05:49 +0900

    Remove kernel

--- lineage.mk
+++ lineage.mk
@@ -2,16 +2,6 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 -include vendor/lineage/build/core/config.mk
 -include vendor/lineage/build/core/apicheck.mk
 
-# Fake Kernel
-BOARD_KERNEL_IMAGE_NAME := Image.lz4
-KERNEL_LD := LD=ld.lld
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := sunfish_defconfig
-TARGET_KERNEL_SOURCE := kernel/google/sunfish
-TARGET_NEEDS_DTBOIMAGE := true
-
 PRODUCT_PACKAGES += \
    FlipFlap \
    NotchBarKiller
